294fe979f72cfdf5df7017e978316733ffc9a9b7
Basavarajrp <53463323+Basavarajrp@users.noreply.github.com>
2021-11-27 15:54:48 +0530

    Added the multipart/form-data route.

--- flaskapi/app.py
+++ flaskapi/app.py
@@ -1,18 +1,18 @@
 from flask import Flask, request
-from flask_restx import Resource, Api, fields, reqparse
+from flask_restx import Resource, Api, reqparse
 import werkzeug
 import cloudinary
 from cloudinary.uploader import upload
 import cloudinary.api
 
 
-# # Cloudainerry configuration
-# cloudinary.config(
-#     cloud_name =  "dzxyqmygj",
-#     api_key =  "379288986521871",
-#     api_secret = "oUwaSB4zZExC7y_qpejm7I6ShwM",
-#     secure = True
-# )
+# Cloudainerry configuration
+cloudinary.config(
+    cloud_name =  "dzxyqmygj",
+    api_key =  "379288986521871",
+    api_secret = "oUwaSB4zZExC7y_qpejm7I6ShwM",
+    secure = True
+)
 
 parser = reqparse.RequestParser()
 app = Flask(__name__)
@@ -25,31 +25,27 @@ parser.add_argument('picture', type=werkzeug.datastructures.FileStorage, locatio
 
 
 
-# @api.route('/hello')
-# class HelloWorld(Resource):
-#     def get(self):
-#         return {'hello': 'world'}
-
-
-# @api.route('/upload')
-# class Upload(Resource):
-#     @api.expect(parser, code=201)
-#     def post(self):
-#         file_to_upload = request.files['picture']
-#         data = request.form.to_dict()
-
-#         if file_to_upload:
-#             upload_result = upload(file_to_upload)
-#             url = upload_result.get("url")
-#             return {'Successfull': 200,
-#                     'data':data, 
-#                     'image':url
-#                    }
-    
 @api.route('/hello')
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}       
+        return {'hello': 'world'}
+
+
+@api.route('/upload')
+class Upload(Resource):
+    @api.expect(parser, code=201)
+    def post(self):
+        file_to_upload = request.files['picture']
+        data = request.form.to_dict()
+
+        if file_to_upload:
+            upload_result = upload(file_to_upload)
+            url = upload_result.get("url")
+            return {'Successfull': 200,
+                    'data':data, 
+                    'image':url
+                   }
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
