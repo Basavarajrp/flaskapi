edde8da55b2db8cc3abe6c886fcec50e8ab1bb9f
Basavarajrp <53463323+Basavarajrp@users.noreply.github.com>
2021-11-27 15:50:28 +0530

    Add files via upload

---
+++ flaskapi/app.py
@@ -0,0 +1,55 @@
+from flask import Flask, request
+from flask_restx import Resource, Api, fields, reqparse
+import werkzeug
+import cloudinary
+from cloudinary.uploader import upload
+import cloudinary.api
+
+
+# # Cloudainerry configuration
+# cloudinary.config(
+#     cloud_name =  "dzxyqmygj",
+#     api_key =  "379288986521871",
+#     api_secret = "oUwaSB4zZExC7y_qpejm7I6ShwM",
+#     secure = True
+# )
+
+parser = reqparse.RequestParser()
+app = Flask(__name__)
+api = Api(app)
+
+# Request Parser
+parser.add_argument('name', type=str, location='form')
+parser.add_argument('last_name', type=str, location='form')
+parser.add_argument('picture', type=werkzeug.datastructures.FileStorage, location='files')
+
+
+
+# @api.route('/hello')
+# class HelloWorld(Resource):
+#     def get(self):
+#         return {'hello': 'world'}
+
+
+# @api.route('/upload')
+# class Upload(Resource):
+#     @api.expect(parser, code=201)
+#     def post(self):
+#         file_to_upload = request.files['picture']
+#         data = request.form.to_dict()
+
+#         if file_to_upload:
+#             upload_result = upload(file_to_upload)
+#             url = upload_result.get("url")
+#             return {'Successfull': 200,
+#                     'data':data, 
+#                     'image':url
+#                    }
+    
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}       
+
+if __name__ == '__main__':
+    app.run(debug=True)
